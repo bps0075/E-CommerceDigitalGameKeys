bdb27357d2e998f18276330a24a994136ed90fd2
Ethan Willinger <EthanWillinger@my.unt.edu>
2023-11-07 15:24:23 -0600

    Added add to cart functionality, the javascript function will check to make sure that the input is a number. The user can change this, but backend will perform additional process to ensure that there are no strings in the user shopping cart array. A test button has been included on the test homepage.html, by default a number is passed as a parameter, but you can try and throw a string in there to see what happens

--- js/item.js
+++
@@ -1,6 +0,0 @@
-class item {
-    constructor(id, price) {
-        this.id = id;
-        this.price = price;
-    }
-}
--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -1,6 +1,14 @@
 const cart = [];
 
-function addToCart(item) {
-    cart.push(item);
+
+//NOTE only numbers should be accepted in this cart
+function addToCart(item_id) {
+    if (typeof item_id != "number") {
+        //alert("INVALID! Not an item")
+        return false;
+    } else {
+    cart.push(item_id);
+    //alert("VALID ITEM")
     return true;
+    }
 }
