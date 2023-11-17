171333d8b4ff9b01f47fa89d51caf94ce461e109
AaronCJacob <77521314+AaronCJacob@users.noreply.github.com>
2023-11-17 14:00:19 -0600

    Update shoppingCart.js

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -1,14 +1,25 @@
 const cart = [];
 
-
-//NOTE only numbers should be accepted in this cart
 function addToCart(item_id) {
-    if (typeof item_id != "number") {
-        //alert("INVALID! Not an item")
-        return false;
-    } else {
+    // Your existing code to add item_id to the cart
     cart.push(item_id);
-    //alert("VALID ITEM")
-    return true;
-    }
+}
+
+function goToPayment() {
+    // Assuming you have a function to redirect to the payment page
+    // Create a form dynamically
+    const form = document.createElement('form');
+    form.method = 'POST';
+    form.action = 'PaymentForm.php'; // Replace with your payment processing PHP file
+
+    // Create a hidden input field to store the cart data
+    const input = document.createElement('input');
+    input.type = 'hidden';
+    input.name = 'shoppingCart';
+    input.value = JSON.stringify(cart); // Convert cart array to a string before sending
+
+    // Append the input to the form and then submit
+    form.appendChild(input);
+    document.body.appendChild(form);
+    form.submit();
 }
